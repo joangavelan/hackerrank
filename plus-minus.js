049733e6028d6cfc6cc1f44e966f65f52555d2bf
Joan Gavelán <joangavelan@gmail.com>
2021-10-21 17:10:03 -0500

    solved

---
+++ plus-minus.js
@@ -0,0 +1,26 @@
+const nums = [-4, 3, -9, 0, 4, 1];
+
+const plusMinus = (nums) => {
+  let length = nums.length;
+  let positives = 0;
+  let negatives = 0;
+  let zeroes = 0;
+
+  for(let i = 0; i < length; i++) {
+    if(nums[i] > 0) {
+      positives++;
+    } else if(nums[i] < 0) {
+      negatives++;
+    } else {
+      zeroes++;
+    }
+  }
+
+  console.log((positives / length).toFixed(6))
+  console.log((negatives / length).toFixed(6))
+  console.log((zeroes / length).toFixed(6))
+}
+
+plusMinus(nums)
+
+// console.log((3 / 6).toFixed(6))
